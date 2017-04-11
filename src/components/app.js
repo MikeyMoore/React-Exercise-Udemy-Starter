2913418d788c4814ccc0c2d10adbde29951bdbd4
MikeyMoore <icarusjedi@gmail.com>
2017-04-11 15:39:24 -0700

    SongList

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import styles from './app.css';
 import SearchBar from './SearchBar/SearchBar.js';
 import searchSpotify from '../utils/searchSpotify';
 import SongItem from './SongItem/SongItem';
+import SongList from './SongList/SongList';
 
 export default class App extends Component {
 
@@ -12,6 +13,7 @@ export default class App extends Component {
       initialMessage: 'greeting',
       song: '',
       tracks: {},
+      songPosition: 0,
     };
   }
 
@@ -21,12 +23,13 @@ export default class App extends Component {
   }
 
   render() {
-    const { tracks } = this.state;
+    const { tracks, songPosition } = this.state;
     console.log('tracks', tracks.tracks);
     return (
       <div className={styles.root}>
         <SearchBar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
-        {tracks.tracks && <SongItem songData={tracks.tracks.items[0]} />}
+        {tracks.tracks && <SongItem songData={tracks.tracks.items[songPosition]} />}
+        {tracks.tracks && <SongList listOfSongs={tracks.tracks.items} selectSong={(songPosition) => this.setState({ songPosition })} />}
       </div>
     );
   }
